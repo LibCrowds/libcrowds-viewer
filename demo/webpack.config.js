e3f2157a14e9ff66526b1bd35c97198cc5dfdd5d
Alex Mendes <alexanderhmendes@gmail.com>
2017-06-26 02:52:39 +0100

    Improve deploy script

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -48,7 +48,6 @@ module.exports = {
 }
 
 if (process.env.NODE_ENV === 'production') {
-  module.exports.devtool = '#source-map'
   // http://vue-loader.vuejs.org/en/workflow/production.html
   module.exports.plugins = (module.exports.plugins || []).concat([
     new webpack.DefinePlugin({
@@ -57,7 +56,6 @@ if (process.env.NODE_ENV === 'production') {
       }
     }),
     new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true,
       compress: {
         warnings: false
       }
