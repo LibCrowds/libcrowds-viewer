b9ec04644809112070749ec7b8d80dd9885b0562
Mendes <alex.mendes@bl.uk>
2017-07-05 15:56:41 +0100

    Update demo webpack config

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/',
-    filename: 'build.js'
+    filename: '[name].js'
   },
   module: {
     rules: [
