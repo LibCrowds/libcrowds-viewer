56822d898160f2a2e26243483c07fc1235c09c8b
Alex Mendes <alexanderhmendes@gmail.com>
2017-07-05 21:07:47 +0100

    Update webpack config

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -77,10 +77,8 @@ if (process.env.NODE_ENV === 'production') {
     new webpack.optimize.UglifyJsPlugin({
       compress: {
         warnings: false
-      }
-    }),
-    new webpack.LoaderOptionsPlugin({
-      minimize: true
+      },
+      sourceMap: true
     })
   ])
 }
