5dd669df1747723f0c00609d2930efa8c6d147cf
Alex Mendes <alex.mendes@bl.uk>
2017-07-17 17:10:37 +0100

    Lint fixes

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -6,9 +6,7 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    publicPath: process.env.NODE_ENV === 'production'
-                  ? './'
-                  : '/',
+    publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
     filename: '[name].[hash].js'
   },
   module: {
