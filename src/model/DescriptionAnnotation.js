88fc2f36379a4eb1c2ed4d2f38443d411f91125a
Alex Mendes <alexanderhmendes@gmail.com>
2017-07-17 02:37:52 +0100

    Add DescriptionAnnotation

---
+++ src/model/DescriptionAnnotation.js
@@ -0,0 +1,36 @@
+import Annotation from '@/model/annotation'
+
+/**
+ * Represents a Web Annotation used for descriptions.
+ * @param {Object} imgInfo
+ *   The IIIF image info.
+ * @param {String} value
+ *   The description value.
+ * @param {String} tag
+ *   The tag value.
+ * @param {String} fragmentURI
+ *   An IIIF image region.
+ * @param {Object} creator
+ *   The Annotation creator.
+ * @param {Object} generator
+ *   The Annotation generator.
+ */
+class DescriptionAnnotation extends Annotation {
+
+  constructor ({
+    imgInfo,
+    value,
+    tag = null, 
+    fragmentURI = null,
+    creator = null, 
+    generator = null
+  }) {
+    super('describing', imgInfo, creator, generator)
+    this.addDescription(value)
+    if (tag) {
+      this.addTag(tag, fragmentURI)
+    }
+  }
+}
+
+export default DescriptionAnnotation
