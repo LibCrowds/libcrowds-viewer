6a4d2a3e3f5964ebdc56a11be600800c375a81c7
Alex Mendes <alex.mendes@bl.uk>
2017-07-17 14:19:03 +0100

    Add coverage and update test config

--- karma.conf.js
+++ karma.conf.js
@@ -4,14 +4,22 @@ module.exports = function (config) {
   config.set({
     browsers: ['PhantomJS'],
     frameworks: ['jasmine'],
-    files: ['test/**/*.js'],
+    files: ['test/index.js'],
+    reporters: ['spec', 'coverage'],
     preprocessors: {
-      'test/**/*.js': ['webpack']
+      'test/index.js': ['webpack', 'sourcemap']
     },
     singleRun: true,
     webpack: webpackConfig,
     webpackMiddleware: {
       noInfo: true
+    },
+    coverageReporter: {
+      dir: './coverage',
+      reporters: [
+        { type: 'lcov', subdir: '.' },
+        { type: 'text-summary' }
+      ]
     }
   })
 }
--- webpack.config.js
+++ webpack.config.js
@@ -27,10 +27,11 @@ module.exports = {
         }
       },
       {
-        test: /\.css$/,
+        test: /\.s?css$/,
         use: [
           'style-loader',
-          'css-loader'
+          'css-loader',
+          'sass-loader'
         ]
       },
       {
