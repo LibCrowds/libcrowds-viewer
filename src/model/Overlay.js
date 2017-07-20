215f5d490cd47813d3305ec23e542305f81c5645
Alex Mendes <alex.mendes@bl.uk>
2017-07-20 19:39:19 +0100

    Add overlay model

---
+++ src/model/Overlay.js
@@ -0,0 +1,22 @@
+/**
+ * Represents an overlay.
+ * @param {String} id
+ *   An ID.
+ * @param {Object} rect
+ *   An OpenSeadragon viewport rectangle.
+ * @param {String} shape
+ *   The type of shape ('rect' or 'circle').
+ */
+class Overlay {
+  constructor ({
+    id,
+    rect,
+    shape = 'rect'
+  }) {
+    this.id = rect
+    this.schema = schema
+    this.shape = shape
+  }
+}
+
+export default Form
