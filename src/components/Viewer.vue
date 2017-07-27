867ae13f5d97240cdf483d1258900623c2d7daa0
Alex Mendes <alex.mendes@bl.uk>
2017-07-27 13:11:40 +0100

    Close old image before loading a new one

--- src/components/Viewer.vue
+++ src/components/Viewer.vue
@@ -578,6 +578,7 @@ export default {
       handler: function (oldVal, newVal) {
         // Update the task image if it has changed
         if (!oldVal || !newVal || oldVal.imgInfoUri !== newVal.imgInfoUri) {
+          this.viewer.close()
           this.viewer.open({
             tileSource: this.currentTask.imgInfoUri,
             success: () => this.configureMode(this.currentTask)
