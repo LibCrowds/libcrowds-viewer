2bc279b2485f14c8516e735995f6daa040232b88
Alex Mendes <alex.mendes@bl.uk>
2017-07-27 20:20:27 +0100

    Update tests

--- test/specs/model/task.spec.js
+++ test/specs/model/task.spec.js
@@ -2,7 +2,7 @@ import Task from '@/model/Task'
 import Annotation from '@/model/Annotation'
 
 describe('Annotation', () => {
-  let selectTask = null
+  let task = null
   let annoOpts = null
   let taskOpts = null
   let annoOne = null
@@ -26,48 +26,56 @@ describe('Annotation', () => {
     }
     annoOne = new Annotation(annoOpts)
     annoTwo = new Annotation(annoOpts)
-    selectTask = new Task(taskOpts)
+    task = new Task(taskOpts)
   })
 
   it('returns an annotation by id', () => {
-    selectTask.annotations = [annoOne]
-    const anno = selectTask.getAnnotation(annoOne.id)
+    task.annotations = [annoOne]
+    const anno = task.getAnnotation(annoOne.id)
     expect(anno).toBe(annoOne)
   })
 
   it('returns undefined when no annotation with a given id', () => {
-    const anno = selectTask.getAnnotation(annoOne.id)
+    const anno = task.getAnnotation(annoOne.id)
     expect(anno).toBe(undefined)
   })
 
   it('stores a new annotation', () => {
-    selectTask.storeAnnotation(annoOne)
-    expect(selectTask.annotations).toEqual([annoOne])
+    task.storeAnnotation(annoOne)
+    expect(task.annotations).toEqual([annoOne])
   })
 
   it('stores multiple new annotations', () => {
-    selectTask.storeAnnotation(annoOne)
-    selectTask.storeAnnotation(annoTwo)
-    expect(selectTask.annotations).toEqual([annoOne, annoTwo])
+    task.storeAnnotation(annoOne)
+    task.storeAnnotation(annoTwo)
+    expect(task.annotations).toEqual([annoOne, annoTwo])
   })
 
   it('updates an existing annotation', () => {
-    selectTask.storeAnnotation(annoOne)
+    task.storeAnnotation(annoOne)
     annoOne.key = '123'
-    selectTask.storeAnnotation(annoOne)
-    expect(selectTask.annotations).toEqual([annoOne])
+    task.storeAnnotation(annoOne)
+    expect(task.annotations).toEqual([annoOne])
   })
 
   it('deletes an annotation', () => {
-    selectTask.annotations = [annoOne]
-    selectTask.deleteAnnotation(annoOne.id)
-    expect(selectTask.annotations).toEqual([])
+    task.annotations = [annoOne]
+    task.deleteAnnotation(annoOne.id)
+    expect(task.annotations).toEqual([])
   })
 
   it('throws an error when no annotation to delete', () => {
     const del = function () {
-      selectTask.deleteAnnotation(annoOne.id)
+      task.deleteAnnotation(annoOne.id)
     }
     expect(del).toThrowError()
+  })
+
+  it('returns annotation from search', () => {
+    task.annotations = [annoOne, annoTwo]
+    const found = task.searchAnnotation({
+      id: annoOne.id
+    })
+    expect(found).toEqual([annoOne])
   })
 })
