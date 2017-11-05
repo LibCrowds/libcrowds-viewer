1877cab4bf4088ec41bb78fdf2c12ca5cd3c7a4c
Alex Mendes <alexanderhmendes@gmail.com>
2017-11-05 14:13:47 +0000

    Draw undrawn selections when window resized (#263)

--- src/components/sidebars/items/Select.vue
+++ src/components/sidebars/items/Select.vue
@@ -89,11 +89,13 @@ export default {
      * Draw the selection canvases.
      */
     drawSelections () {
+      console.log(this.tags)
       for (let tag of this.tags) {
         let destCanvas = this.$refs[`canvas-${tag.id}`][0]
         if (
           typeof destCanvas !== 'undefined' &&
-          destCanvas.hasAttribute('drawn')
+          destCanvas.hasAttribute('drawn') &&
+          destCanvas.width > 0
         ) {
           continue
         }
@@ -138,7 +140,15 @@ export default {
   },
 
   mounted () {
-    this.drawSelections()
+    if (typeof window !== 'undefined') {
+      window.addEventListener('resize', this.drawSelections)
+    }
+  },
+
+  beforeDestroy () {
+    if (typeof window !== 'undefined') {
+      window.removeEventListener('resize', this.drawSelections)
+    }
   }
 }
 </script>
